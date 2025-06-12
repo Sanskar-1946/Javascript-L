a96740f10c7e7ea38f9110107bcdc827ab451bd0
Sanskar-1946 <sanskarghule9@gmail.com>
2025-06-12 14:22:32 +0000

    container config done

---
+++ 01_Basics/Base.js
@@ -0,0 +1,16 @@
+const accountId = 44444
+let accountEmail = "sanskar44@google.com"
+var accountPassword = "56789"
+accountCompany = "concubes"
+var accountPost
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCompany, accountPost]);
+/*
+for variable two keywords are used 
+var and let
+prefer not to use var bcoz of block scope and functional scope
+const is used for constants
+console.table() is used to make table 
+variable can also be given without using keyword
+*/
